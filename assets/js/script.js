bb2de85035996fbd2f2e5f7e1a8e9b88e68767ff
carocuevas <catecuevas@gmail.com>
2018-08-15 20:38:49 -0300

    noveno commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,42 @@
-function mycarousel_initCallback(carousel) {
-			
-    jQuery('#forw').bind('click', function() {
-        carousel.next();
-        return false;
-    });
+(function($){
 
-    jQuery('#prev').bind('click', function() {
-        carousel.prev();
-        return false;
-    });
-};
+	function mycarousel_initCallback(carousel) {
 
-jQuery(document).ready(function() {
-    jQuery("#carrusel").jcarousel({
-       scroll :1,
-	   auto: 0,
-       wrap : "both",
-       initCallback: mycarousel_initCallback,
-	    buttonNextHTML: null,
-        buttonPrevHTML: null
-	   });
-});
+		$('#forw').bind('click', function() {
+			carousel.next();
+			return false;
+		});
+
+		$('#prev').bind('click', function() {
+			carousel.prev();
+			return false;
+		});
+	};
+
+	$("#carrusel").jcarousel({
+		scroll :1,
+		auto: 0,
+		wrap : "both",
+		initCallback: mycarousel_initCallback,
+		buttonNextHTML: null,
+		buttonPrevHTML: null
+	});
+
+})(jQuery);
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+
+$(document).ready() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
+  });
+ });
